3c79b8ad30ba668b22fc8086f9643328c76f888e
thonghm <thong.haminh@floatingcube.com>
2026-01-29 10:15:45 +0700

    update limit export orders

--- assets/admin/js/Components/Pages/Orders/OrdersTable.js
+++ assets/admin/js/Components/Pages/Orders/OrdersTable.js
@@ -63,7 +63,7 @@ const OrdersTable = ({ orders, orderBy, orderDirection, handleSort }) => {
     setSelectedOrders((prev) =>
       prev.includes(String(orderId))
         ? prev.filter((x) => x !== String(orderId))
-        : [...prev, String(orderId)]
+        : [...prev, String(orderId)],
     );
   };
 
@@ -244,7 +244,7 @@ const OrdersTable = ({ orders, orderBy, orderDirection, handleSort }) => {
         onPageChange={handleChangePage}
         rowsPerPage={rowsPerPage}
         onRowsPerPageChange={handleChangeRowsPerPage}
-        rowsPerPageOptions={[5, 10, 20]}
+        rowsPerPageOptions={[5, 10, 20, 50, 100, 200]}
       />
     </Paper>
   );
